e0cb211b0cc0f763a8d7047b2a45774c8820a75e
james chalmers <james.chalmers184@gmail.com>
2022-07-29 12:00:13 +0100

    popups added to edit and details screens

--- CustomerPortalRazor/Data/Models/Status.cs
+++ CustomerPortalRazor/Data/Models/Status.cs
@@ -1,8 +1,13 @@
-﻿namespace CustomerPortalRazor.Data.Models
+﻿using System.ComponentModel;
+
+namespace CustomerPortalRazor.Data.Models
 {
     public enum Status
     {
+        [Description("Active")]
         Active,
+
+        [Description("Inactive")]
         Inactive
     }
 }
